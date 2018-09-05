11c85ed2371020fdd5bfd5ef77fc2bed3965f36e
Alvin <alvingho25@gmail.com>
2018-09-05 13:57:15 +0700

    add computer score for rps game

--- rps.js
+++ rps.js
@@ -36,16 +36,18 @@ function random(){
     }
 }
 
-var score = 0;
-for(var i = 0; i < 5; i++){
+var playerScore = 0;
+var computerScore = 0;
+for(var i = 0; i < 10; i++){
     playerSelection = prompt(`Round ${i+1} Choose rock or paper or scissors`).toLowerCase();
     computerSelection = random();
     let hasil = play(playerSelection, computerSelection);
     if(hasil == "win"){
-        score++
+        playerScore++
         console.log(`You Win! ${playerSelection} beat ${computerSelection}`)
     }
     else if(hasil == "lose"){
+        computerScore++;
         console.log(`You Win! ${computerSelection} beat ${playerSelection}`)
     }
     else{
@@ -53,4 +55,14 @@ for(var i = 0; i < 5; i++){
         i--;
     }
 }
-console.log(`Round End! Your Score: ${score}`)
+
+console.log(`Player ${playerScore} against Computer ${computerScore}`)
+if(playerScore > computerScore){
+    console.log("Congratulations you win the game")
+}
+else if(playerScore < computerScore){
+    console.log("BOOOOO you lose against computer? pathetic")
+}
+else{
+    console.log("not bad you draw against computer")
+}
